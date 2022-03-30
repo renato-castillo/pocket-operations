053ff7ba39f8dafabd31b1d6c0659fe8fe63ce46
renato-castillo <renatocastillopingo@gmail.com>
2022-03-30 17:23:05 -0500

    first commit

---
+++ src/main/java/com/bootcamp/pocketoperations/PocketOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.pocketoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/pocketoperations/PocketOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.pocketoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
